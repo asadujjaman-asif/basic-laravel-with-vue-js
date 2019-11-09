a4432821ea7ffe5d5f96274c2debeeed8d6abdd5
Asadujjaman Asif <az.asifislam@gmail.com>
2019-11-10 00:17:26 +0600

    create middleware

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,16 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 /*Route::get('/{anypath}', 'HomeController@index')->where('path','.*');*/
 //Category
-Route::post('/add-category','CategoryController@add_category');
-Route::get('/category','CategoryController@get_category_list');
-Route::get('/category/{id}','CategoryController@delete_category');
-Route::get('/edit-category/{id}','CategoryController@edit_category');
-Route::post('/update-category/{id}','CategoryController@update_category');
-//get post data
-Route::get('/post','PostController@allpost');
-Route::post('/save-new-post','PostController@saveNewPost');
-Route::get('/get-post-by-id/{id}','PostController@editPost');
-Route::get('/delete-post/{id}','PostController@postdelete');
-Route::post('/update-post/{id}','PostController@updatePost');
+Route::group(['middleware'=>['auth']], function(){
+	Route::post('/add-category','CategoryController@add_category');
+	Route::get('/category','CategoryController@get_category_list');
+	Route::get('/category/{id}','CategoryController@delete_category');
+	Route::get('/edit-category/{id}','CategoryController@edit_category');
+	Route::post('/update-category/{id}','CategoryController@update_category');
+	//get post data
+	Route::get('/post','PostController@allpost');
+	Route::post('/save-new-post','PostController@saveNewPost');
+	Route::get('/get-post-by-id/{id}','PostController@editPost');
+	Route::get('/delete-post/{id}','PostController@postdelete');
+	Route::post('/update-post/{id}','PostController@updatePost');
+});
